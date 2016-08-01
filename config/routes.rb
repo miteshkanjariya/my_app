0647c3807c145202f18dea4fc4243e1f736d3f92
mitesh.kanjariya <mitesh.kanjariya@gmail.com>
2016-07-31 21:39:10 -0700

    fixing errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :products
-  resources :PaymentNotification
+  resources :payment_notification
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 end
