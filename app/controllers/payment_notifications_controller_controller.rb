6b5a17d1765c5dd3df69af6594d83ce44648bf48
mitesh.kanjariya <mitesh.kanjariya@gmail.com>
2016-07-31 22:19:32 -0700

    updated paypal_url

--- app/controllers/payment_notifications_controller_controller.rb
+++ app/controllers/payment_notifications_controller_controller.rb
@@ -1,6 +1,7 @@
 class PaymentNotificationsController < ApplicationController
   protect_from_forgery :except => [:create]
   def create
+    Rails.logger.debug("*******************got pay-pal notification************************")
     PaymentNotification.create!(:params => params, :status => params[:payment_status], :transaction_id => params[:txn_id] )
     render :nothing => true
   end
